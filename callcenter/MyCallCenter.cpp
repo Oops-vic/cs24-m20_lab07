b45b354a9d8f27f87d563c1a96c8313fdc00f2f2
Chen Shen <chen_shen@ucsb.edu>
2020-08-16 02:51:44 -0700

    Some basic work done

--- callcenter/MyCallCenter.cpp
+++ callcenter/MyCallCenter.cpp
@@ -1,7 +1,84 @@
 #include "MyCallCenter.h"
+#include <algorithm>
+#include <iostream>
 
 CallCenter* CallCenter::create(std::vector<Employee> employees) {
-  return new MyCallCenter(/* TODO */);
+    return new MyCallCenter(employees);
 }
 
 // MyCallCenter Member Functions
+MyCallCenter::MyCallCenter(std::vector<Employee> employeeInfo){
+    this->mEmployees = employeeInfo;
+    this->EmployeeSorted = employeeInfo;
+    std::sort((this->EmployeeSorted).begin(), (this->EmployeeSorted).end(), this->sortBySkill);
+}
+
+MyCallCenter::~MyCallCenter(){
+
+}
+
+bool MyCallCenter::sortBySkill(const Employee& lhs, const Employee& rhs){
+    return (lhs.skill < rhs.skill);
+}
+
+int MyCallCenter::findexpWeightedWaitTime(Call c){
+    int result;
+    result = c.importance * (this->time + c.work_required - c.work_performed + 1 - c.recieved);
+    return result;
+}
+
+bool MyCallCenter::sortByWeightedWaitTime(const Call& lhs, const Call& rhs){
+    int leftWeight, rightWeight;
+    leftWeight = this->findexpWeightedWaitTime(lhs);
+    rightWeight = this->findexpWeightedWaitTime(rhs);
+    return (leftWeight < rightWeight);
+}
+
+int MyCallCenter::findAble(Call* c){
+    for (auto itr = (this->EmployeeSorted).begin(); itr != (this->EmployeeSorted).end(); itr++){
+        if (itr->skill >= c->difficulty) return (itr->id);
+    }
+    return -1;
+}
+
+std::vector<int> MyCallCenter::distribute(){
+    int ansLen = (this->answered).size();
+    for (int i = ansLen - 1; i >= 0; i--){
+        int empId = this->findAble(this->answered[i]);
+        Employee emp = this->mEmployees[empId];
+        if (!emp.call){
+            
+        }
+    }
+}
+
+std::vector<int> MyCallCenter::calls(int minute, const std::vector<int>& call_ids){
+    this->time = minute;
+    for (auto id: call_ids){
+        (this->unanswerd).push(id);
+    }
+    std::vector<int> result = this->distribute();
+
+    for (auto itr = result.begin(); itr != result.end(); itr++){
+        if (*itr == 0 && (this->unanswerd).size() > 0){                     //Answer unanswered calls if there's remaining employees
+            *itr = (this->unanswerd).front();
+            (this->unanswerd).pop();
+            continue;
+        }
+        if (*itr == -1){                                                    //Finish calls
+            Call cFin = this->mEmployees[itr - result.begin()].call;
+            this->mEmployees[itr - result.begin()].call = nullptr;
+            delete cFin;
+        }
+    }
+    return result;
+}
+
+void MyCallCenter::learn(int minute, const std::vector<Call>& calls){
+    this->time = minute;
+    for (auto call: calls){
+        Call c = new Call(call);
+        (this->answered).push_back(temp);
+    }
+    std::sort((this->answered).begin(), (this->answered).end(), this->sortByWeightedWaitTime);
+}
--- callcenter/MyCallCenter.h
+++ callcenter/MyCallCenter.h
@@ -2,18 +2,38 @@
 #define MYCALLCENTER_H
 
 #include "CallCenter.h"
+#include <queue>
 
 class MyCallCenter: public CallCenter {
-  // Member Variables
+    // Member Variables
+    std::vector<Employee> mEmployees;                                   //Employees listed by id numbers (start 0)
+    std::vector<Employee> EmployeeSorted;                               //Employees sorted by skills (from low to high)
+    std::queue<int> unanswerd;                                          //Calls that goes straight into the pool
+                                                                        //(only know call_id)
+    std::vector<Call*> answered;                                        //Calls that has been answered and put to pool
+                                                                        //(assume sorted by importance * wait_time from low to high)
+    int time;                                                           //Current time
+
+private:
+    int findAble(Call* c);                                              //Find the employee id just able to solve the call
+                                                                        //(return -1 for not find, not possible to trigger)
+    int findexpWeightedWaitTime(Call c);                                //Calculate the expected weighted wait time
+    bool sortBySkill(const Employee& lhs, const Employee& rhs);
+    bool sortByWeightedWaitTime(const Call& lhs, const Call& rhs);      //Compare the expected weighted wait time
+    void addInCall(std::vector<int>& vec, int indexVec, int indexAns);
+
 public:
-  // Constructor
-  // Destructor?
+    // Constructor
+    MyCallCenter(std::vector<Employee> employeeInfo);
+    // Destructor?
+    ~MyCallCenter();
 
-  // Required Member Functions
-  std::vector<int> calls(int minute, const std::vector<int>& call_ids);
-  void learn(int minute, const std::vector<Call>& calls);
+    // Required Member Functions
+    std::vector<int> calls(int minute, const std::vector<int>& call_ids);
+    void learn(int minute, const std::vector<Call>& calls);
 
-  // Other Member Functions
+    // Other Member Functions
+    std::vector<int> distribute();                                      //Distribute calls in answered vector to employees
 };
 
 #endif
